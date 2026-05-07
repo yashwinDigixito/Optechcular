e3b8dbbb44b139742569e8d09466f24203900a3d
Yashwin <yashwin.sharma@digixito.org>
2026-05-07 21:52:58 +0530

    Initial change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,55 @@
-import "./globals.css";
-
-import Sidebar from "@/component/sidebar/Sidebar";
+"use client";
 
 import Navbar from "@/component/navbar/Navbar";
-
+import Sidebar from "@/component/sidebar/Sidebar";
+import AuthProvider from "@/providers/AuthProvider";
 import Providers from "@/providers/Providers";
+
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
+  const isLoginPage = pathname === "/login";
+
   return (
     <html lang="en">
       <body>
         <Providers>
-          <div
-            style={{
-              display: "flex",
-            }}
-          >
-            <Sidebar />
+          <AuthProvider>
+            {isLoginPage ? (
+              children
+            ) : (
+              <div style={{ display: "flex" }}>
+                <Sidebar />
 
-            <div
-              style={{
-                flex: 1,
-                minHeight: "100vh",
-                background: "#F4F7FE",
+                <div
+                  style={{
+                    flex: 1,
+                    minHeight: "100vh",
+                    background: "#F4F7FE",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Navbar />
 
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <Navbar />
-
-              <main
-                style={{
-                  flex: 1,
-                  padding: "24px",
-                }}
-              >
-                {children}
-              </main>
-            </div>
-          </div>
+                  <main
+                    style={{
+                      flex: 1,
+                      padding: "24px",
+                    }}
+                  >
+                    {children}
+                  </main>
+                </div>
+              </div>
+            )}
+          </AuthProvider>
         </Providers>
       </body>
     </html>
